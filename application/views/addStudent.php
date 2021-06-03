064a6310fe168d25ded1c4b98ea0a9c84828cac9
Gihozo fabrice <77930609+fabrosg@users.noreply.github.com>
2021-06-03 13:29:18 -0700

    Update addStudent.php

--- application/views/addStudent.php
+++ application/views/addStudent.php
@@ -37,7 +37,7 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="collegename" class="col-md-3 control-label">College Name</label>
+					<label for="collegename" class="col-md-3 control-label">IPRC Name</label>
 					<select class="col-md-9" name="college_id">
 						<option value="">Select</option>
 						<?php if (count($colleges)): ?>
